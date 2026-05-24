4521ab994aec9c2fdf420029285fa6e308cbe45b
Chiara Mazzei <mazzei.c@northeastern.edu>
2026-05-24 15:51:00 +0200

    First working version

--- dartboard.py
+++ dartboard.py
@@ -23,37 +23,47 @@ class DartBoard:
 
         return r, theta
 
-    def get_score(self, hit_loc: list[float]) -> tuple[int, str]:
+    def get_score(self, hit_loc: list[float]) -> tuple[int, str, int]:
         # convert dart position to polar coordinates 
         r, theta = self.compute_polar_coordinates(x=hit_loc[0], y=hit_loc[1])
-
-        # from a class variable determine which section of the dartboard the dart landed in
         
         for key, value in BoardMeasurements.segment_radii.items():
             if value[0] < r <= value[1]:
                 segment_type = key
             elif r > 170: 
-                return 0, "out"
+                return 0, "out", 0
             elif r == 0:
-                return 50, "bull"
+                return 50, "bull", 50
 
         for key, value in BoardMeasurements.segment_angles.items():
             if value[0] < theta <= value[1]:
                 if segment_type == "bull":
-                    return 50, "bull"
+                    return 50, "bull", 50
                 elif segment_type == "outer_bull":
-                    return 25, "outer_bull"
+                    return 25, "outer_bull", 25
                 else:
-                    segment_number = key
+                    if key == "6+" or key == "6-":
+                        segment_number = 6
+                    else:
+                        segment_number = key
             elif theta == 0:
                 if segment_type == "bull":
-                    return 50, "bull"
+                    return 50, "bull", 50
                 elif segment_type == "outer_bull":
-                    return 25, "outer_bull"
-                segment_number = 6
+                    return 25, "outer_bull", 25
+                else:
+                    segment_number = 6
+
+        # compute scored value of the dart
+        if segment_type == "treble":
+            score = 3 * segment_number
+        elif segment_type == "double":
+            score = 2 * segment_number
+        else:
+            score = segment_number # bull already has 50 as the number        
         
         # return the number and "double"/"triple" segment
-        return segment_number, segment_type
+        return score, segment_type, segment_number
     
 
     
--- dartbot.py
+++ dartbot.py
@@ -1,27 +1,51 @@
 import math
 import random
-from dartboard import DartBoard
+
+from aim_points import aim_points
 from board_measurements import BoardMeasurements
+from checkout_routes import checkout_routes
+from dartboard import DartBoard
 
 
 class DartBot:
 
     def __init__(self, aim_skill: list):
+        self.dartboard = DartBoard()
         self.sigma_x = aim_skill[0]
         self.sigma_y = aim_skill[1]
 
-    def throw_dart(self, aim_point):
+    def think(self, remaining_score: int) -> tuple[str, tuple]:
+        try:
+            aim_at = checkout_routes[remaining_score][0]
+        except KeyError as e:
+            aim_at = "T20"
+
+        aim_coordinates = aim_points[aim_at]
+
+        return aim_at, aim_coordinates
+
+    def throw_dart(self, aim_point: tuple) -> tuple[int, str, int]:
         # generate randomly how far away you are from aiming point
         x_dist_from_aimpoint = random.normalvariate(mu=0, sigma=self.sigma_x)
         y_dist_from_aimpoint = random.normalvariate(mu=0, sigma=self.sigma_y)
 
-        # compute position of thrown dart
-        x = aim_point[0] + x_dist_from_aimpoint
-        y = aim_point[1] + y_dist_from_aimpoint
+        # unpack aim point
+        aim_x, aim_y = aim_point
 
-        return x, y
+        # compute position of thrown dart
+        x = aim_x + x_dist_from_aimpoint
+        y = aim_y + y_dist_from_aimpoint
+
+        # find which segment was hit, and if it was a treble, double, or single
+        score, segment_type, segment_number = self.dartboard.get_score(hit_loc=[x, y])
+
+        return score, segment_type, segment_number
     
-    # bot needs to know where to aim
-    # bot needs the centers of all triple and double zones
-    # bot needs the checkout routes
+    def turn(self, remaining_score: int) -> tuple[int, str, int]:
+        aim_at, aim_coordinates = self.think(remaining_score)
+        score, segment_type, segment_number = self.throw_dart(aim_coordinates)
+        return aim_at, score, segment_type, segment_number
+
+
+
 
